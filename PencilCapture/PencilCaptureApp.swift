8e3db0c0f69d37ee90bcb5213bdf61b96a5f34b6
IntekhabAhmad <intekhab471@gmail.com>
2026-06-25 15:18:57 -0500

    Initial Commit

---
+++ PencilCapture/PencilCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilCaptureApp.swift
+//  PencilCapture
+//
+//  Created by research2 on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct PencilCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
